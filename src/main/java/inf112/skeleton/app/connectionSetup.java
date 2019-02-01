1249e1d744e71f0c982a622f566d8b0e45f57699
Jørgen Meinich <jme001@hotmail.no>
2019-02-01 14:05:54 +0100

    forsøk på å legge grunnlaget til multiplayer funksjonalitet. tldr:gjør absolut ingen ting for øyeblikket.

---
+++ src/main/java/inf112/skeleton/app/connectionSetup.java
@@ -0,0 +1,47 @@
+package inf112.skeleton.app;
+
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.net.UnknownHostException;
+import java.util.Scanner;
+import java.net.InetAddress;
+
+    //prøver å etablere lan connection tingene
+public class connectionSetup {
+
+    private int decidedPort=9090;
+    private String multiplayerRole;
+    private String server="primary";
+    private String client="secondary";
+    private InetAddress serverIP;
+
+    public connectionSetup(String multiplayerRole) {
+        Scanner reader = new Scanner(System.in);
+        System.out.println("if client write secondary, if server write primary: ");
+        multiplayerRole = reader.next();
+        reader.close();
+
+        if(multiplayerRole.equals(server)){
+            try {
+                ServerSocket ServerSide = new ServerSocket(decidedPort);
+            } catch (IOException e) { e.printStackTrace(); }
+
+            try {
+                serverIP=InetAddress.getLocalHost();
+            } catch (UnknownHostException e) { e.printStackTrace(); }
+
+        }
+
+
+        else if(multiplayerRole.equals(client)){
+            try {
+                Socket ClientSide = new Socket(serverIP, decidedPort);
+            } catch (IOException e) { e.printStackTrace(); }
+
+        }
+    }
+
+
+
+}
