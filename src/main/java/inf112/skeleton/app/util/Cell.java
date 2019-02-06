92f540e45b2c7762a3626785e24d157b82cbbb1c
Ditlef Diseth <ditlef.diseth@student.uib.no>
2019-02-06 17:56:00 +0100

    test

--- src/main/java/inf112/skeleton/app/util/Cell.java
+++ src/main/java/inf112/skeleton/app/util/Cell.java
@@ -3,5 +3,5 @@ package inf112.skeleton.app.util;
 /**
  * Created by Martin on 06/02/2019.
  */
-public class Tile {
+public class Cell {
 }
--- src/main/java/inf112/skeleton/app/util/MyGrid.java
+++ src/main/java/inf112/skeleton/app/util/MyGrid.java
@@ -3,14 +3,16 @@ package inf112.skeleton.app.util;
 /**
  * Created by Martin on 06/02/2019.
  */
-public class Grid {
+public class MyGrid {
 
     private int width;
     private int length;
     private int[][] board;
 
 
-    public Grid()
+    public MyGrid() {
+
+    }
 
 
 
