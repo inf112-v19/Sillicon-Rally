adbc259dbcaacb6c9048dd671c300ad7e0f452d8
Jørgen Meinich <jme001@hotmail.no>
2019-02-06 15:28:08 +0100

    forsøker å legge ned grunnlaget til å lese ut tiledata fra mappene og la til noen "todo" meldinger

---
+++ src/main/java/inf112/skeleton/app/hazards/BoardHazards.java
@@ -0,0 +1,47 @@
+package inf112.skeleton.app.hazards;
+
+public class BoardHazards {
+
+    public void Mountain(){
+    }
+
+    public void Pit(){
+        // "upon moving onto pit tile"
+        // add 2x spam dmg type cards
+        // end turn
+        // move robot to reboot token/tile
+    }
+
+    public void Laser(){
+        // "at turn end"
+        // add 1x spam dmg pr laser (1-3)
+        // cannot pass through player/robots or walls
+        // priority 3
+
+        int direction;
+        int amount;
+    }
+
+    public void Gears(){
+        // "at turn end"
+        // rotate robot 90d left or right
+        // priority 2
+
+        int rotation;
+    }
+
+    public void Conveyors(){
+        // "at turn end"
+        // move robot in direction
+        // priority 1
+
+        int direction;
+    }
+
+    public void rebootTile(){
+        //position
+        int x;
+        int y;
+    }
+
+}
