5dcee4760bff43b5f0e60c6e1c792fae65ae02f0
Vegard <varneberg@gmail.com>
2019-03-08 14:02:35 -0500

    added branch flagtest with class flagtest

---
+++ src/test/java/inf112/skeleton/app/TeleportTests/FlagTest.java
@@ -0,0 +1,29 @@
+/*
+package inf112.skeleton.app.TeleportTests;
+
+import inf112.skeleton.app.Objects.Player;
+import inf112.skeleton.app.SetupVariables.SetupVariables;
+import inf112.skeleton.app.collision.objects.FlagObject;
+import inf112.skeleton.app.grid.Tile;
+import inf112.skeleton.app.grid.TileGrid;
+import org.junit.jupiter.api.Test;
+
+
+public class FlagTest {
+
+    @Test
+   public void flagTest(){
+       SetupVariables variables = new SetupVariables();
+       TileGrid grid = variables.grid;
+       FlagObject flag = variables.flag;
+       Player player = variables.player;
+
+       player.setPosition(flag.yLocation, flag.xLocation, grid);
+       flag.handleCollision(player, grid);
+
+
+
+        variables.stop();
+    }
+}
+*/
