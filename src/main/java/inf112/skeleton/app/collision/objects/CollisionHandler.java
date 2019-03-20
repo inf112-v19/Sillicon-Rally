7b67c9e22b4c3636aad04bb85a05811925901572
jme011 <jme001@hotmail.no>
2019-03-20 15:29:51 +0100

    adding laser collision handler for lasers

--- src/main/java/inf112/skeleton/app/collision/objects/CollisionHandler.java
+++ src/main/java/inf112/skeleton/app/collision/objects/CollisionHandler.java
@@ -35,6 +35,9 @@ public class CollisionHandler {
             if (spritesOnTile.get(i) instanceof ConveyorBeltObject){
                 ((ConveyorBeltObject) spritesOnTile.get(i)).handleCollision(player, grid);
             }
+            /*if (spritesOnTile.get(i) instanceof LaserObject){
+                ((LaserObject) spritesOnTile.get(i)).handleCollision(player, grid);
+            }*/
         }
 
     }
