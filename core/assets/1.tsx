3b510ec72f533c16583296e54d38a44cfb6ab04a
jme011 <jme001@hotmail.no>
2019-04-03 15:01:58 +0200

    fixed flag graphics showing pickup order.

---
+++ core/assets/1.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.2.2" name="1" tilewidth="96" tileheight="96" tilecount="1" columns="1">
+ <image source="1.png" width="96" height="96"/>
+</tileset>
